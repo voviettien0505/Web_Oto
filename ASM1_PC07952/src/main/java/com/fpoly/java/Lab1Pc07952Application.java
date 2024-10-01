500a82a4c65d2047d7135a276febab091c1ddf6f
huypro123511 <huyntpc08049@fpt.edu.vn>
2024-10-01 18:52:29 +0700

    hhhh

---
+++ ASM1_PC07952/src/main/java/com/fpoly/java/Lab1Pc07952Application.java
@@ -0,0 +1,13 @@
+package com.fpoly.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Pc07952Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1Pc07952Application.class, args);
+	}
+
+}
---
+++ ASM1_PC07952/src/test/java/com/fpoly/java/Lab1Pc07952ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpoly.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1Pc07952ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
